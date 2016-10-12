a78964abf758e8e7f84e9eaf2e1678fb4cf30fa0
EQuimper <quimperemanuel@gmail.com>
2016-10-11 21:46:13 -0400

    Add react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Button bsStyle="primary" bsSize="large">Primary</Button>
         </p>
       </div>
     );
